01ed752d049a172f4d3d3a4bf62cc7cd05382ce2
Zane Stickel <zanesaws@amazon.com>
2022-01-30 21:30:26 -0500

    Added iOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftTwitterAPIV2Client",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),.iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
